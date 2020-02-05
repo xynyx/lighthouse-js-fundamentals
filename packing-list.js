3a338de95b2083f788b39c9f4a8239d6645a9d0f
Matt Taylor <matt.taylr22@gmail.com>
2020-02-05 20:19:26 +0000

    packing list example; added while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,17 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils"];
 
+
+// For Loop
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+
+// While Loop
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length){
   console.log(packingList[i]);
+  i++;
 }
