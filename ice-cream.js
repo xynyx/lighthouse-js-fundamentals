c6b53e6cfb5775cea0d2d617f7c8e726a92f8fb5
Matt Taylor <matt.taylr22@gmail.com>
2020-02-05 20:17:01 +0000

    lighthouse array exercises

---
+++ ice-cream.js
@@ -0,0 +1,22 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
+
+
+
+
+// Option 2 correct
+// 5 + "3"
+
+// Using the + operator on a number and a string will convert the number into a string and concatenate them, resulting in a new string.
+
+// Unlike if you use * or /, it will convert the string into a number instead
---
+++ lighthouseArrays.js
@@ -0,0 +1,12 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+console.log("My favourite " + director + "film is " + films[1]);
+
+// Trying to access index of array that doesn't exist? => undefined
+
+const oddNumbers = [1, 3, 5, 7, 9];
+const randomStuff = [1.15, "bicycle", true]; // Mixed array; not best practice
+
+
+
